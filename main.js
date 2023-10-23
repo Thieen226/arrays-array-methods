fa68109837b7e0f59710f45af5149e928f360db1
Thieen226 <t.ho1@my.metroed.net>
2023-10-23 10:24:08 -0700

    add

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var names = ["Thien", "James", "Hannah", "Katy", "Bang"];
 
 function push(){
     for(let i = 0; i < names.length; i++){
-        console.log[i];
+        console.log([i]);
     }
 }
