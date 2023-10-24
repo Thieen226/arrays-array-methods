1dbea8fca8c6a48c8d70c050da731868523d3f65
Thieen226 <t.ho1@my.metroed.net>
2023-10-24 08:00:02 -0700

    add

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var names = ["Thien", "James", "Hannah", "Katy", "Bang"];
 
-function push(){
+//allowing to add one or more elements into names
+function push(names, ...elements){
     for(let i = 0; i < names.length; i++){
         console.log(i);
     }
