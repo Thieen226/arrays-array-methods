f219efff69a3189ef5c97bc54a9377abd5ff29ab
Thien Ho <t.ho1@my.metroed.net>
2023-10-24 22:27:16 -0700

    add

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var names = ["Thien", "Kim", "Katy", "Hannah"];
 
+//creating push function with a variable in () to add a variable at the end of the array
 //item is variable you want to add into the array
 function push(item){
-    
-    
+    /*add the item you want to add at the end of the array 
+    by making the index of array equal to the length of array
+    */
     names[names.length] = item;
 }
 
@@ -11,8 +13,12 @@ function push(item){
 push("Duyen");
 console.log(names);
 
-
+//creating pop function to remove the last variable in the array
 function pop(){
+
+    /*removing the last item in the array by making the index of the array equal to the length of the array 
+     but the length of the array will less than the intial length by 1 
+    */
     names[names.length -1];
 }
 
@@ -20,6 +26,7 @@ function pop(){
 pop();
 console.log(names); 
 
+// creating toString function which will make the variable in the array to string
 function toString(){
 
 }
