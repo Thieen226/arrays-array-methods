8dc28a38158bd1288f526e7f697b59a780174d9c
Thien Ho <t.ho1@my.metroed.net>
2023-10-24 21:44:56 -0700

    add

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var names = ["Thien", "Kim", "Katy", "Hannah"];
 
+//item is variable you want to add into the array
 function push(item){
+    
+    
     names[names.length] = item;
 }
 
+//adding "Duyen" into names array and excute the array in console
 push("Duyen");
 console.log(names);
 
@@ -12,5 +16,10 @@ function pop(){
     names[names.length -1];
 }
 
+//removing the last string in the array and execute the array in console
 pop();
-console.log(names);     
+console.log(names); 
+
+function toString(){
+
+}
